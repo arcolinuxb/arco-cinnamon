37c8c203f1e79f995f198e7431da35359162b073
arcolinuxz <arcolinuxinfo@gmail.com>
2023-07-12 10:28:39 +0200

    new calamares from 23 07 12

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="cinnamon"
 	dmDesktop="cinnamon"
 
-	arcolinuxVersion='v23.09.01'
+	arcolinuxVersion='v23.09.02'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
