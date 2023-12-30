170da011f4695a75a0644e2ef586b9901ec1357e
arcolinuxz <arcolinuxinfo@gmail.com>
2023-12-30 08:38:14 +0100

    bump nr

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="cinnamon"
 	dmDesktop="cinnamon"
 
-	arcolinuxVersion='v24.01.04'
+	arcolinuxVersion='v24.02.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
